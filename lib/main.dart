2dcf0cb09fa848d61774d41515eb33e530096153
EricBL3 <ericbuitron3@gmail.com>
2020-10-13 19:59:45 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  String _textValue = '';
+
+  void _updateTextFieldValue(String val)
+  {
+    print(val);
+    _textValue = val;
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("State on Stateless Widget"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                onChanged: (value) => _updateTextFieldValue(value),
+              ),
+              SizedBox(height: 10.0,),
+              Text(_textValue),
+            ],
+          )
+        )
+      ),
+    );
+  }
+}
+
